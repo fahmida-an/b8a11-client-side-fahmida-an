0c0113773fd2e77c82c2a48048ea37321345b4c0
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-04 23:08:05 +0600

    Flowbite and Font setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif"
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
