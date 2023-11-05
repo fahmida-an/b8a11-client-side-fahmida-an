2f6074b34fd792778bb89a95e15cd648e802b53b
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-11-05 15:32:46 +0600

    Added Banner

--- src/layout/home/Home.jsx
+++ src/layout/home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../../pages/banner/Banner";
 import NavBar from "../../pages/navbar/NavBar";
 
 const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Banner></Banner>
         </div>
     );
 };
